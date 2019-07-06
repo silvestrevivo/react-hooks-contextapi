75e4768f5d1d6cbb9ecd4bec94ae1781c367135b
silvestrevivo <silvestrevivomillan@gmail.com>
2019-07-06 14:52:48 +0200

    useContext hook replacing old implementation

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,7 +1,27 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
-class BookList extends Component {
+const BookList = () => {
+  const { isLightTheme, light, dark } = useContext(ThemeContext);
+  const theme = isLightTheme ? light : dark;
+
+  return(
+    <div className="book-list" style={{color: theme.syntax, background: theme.bg}}>
+      <ul>
+        <li style={{background: theme.ui}}>the way of kings</li>
+        <li style={{background: theme.ui}}>the name of the wind</li>
+        <li style={{background: theme.ui}}>the final empire</li>
+      </ul>
+    </div>
+  )
+}
+
+export default BookList;
+
+/*
+This is the reference as Class component
+cl
+ass BookList extends Component {
   static contextType = ThemeContext;
 
   render() {
@@ -20,3 +40,4 @@ class BookList extends Component {
 }
 
 export default BookList;
+*/
